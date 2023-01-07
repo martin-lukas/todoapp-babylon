86cfcc975ccae46f3ee9847af91774796c61c0cf
Martin Lukas <martinlukas24@protonmail.com>
2023-01-07 02:12:06 +0000

    Implement the most basic HTTP server in Java

--- backend-java/src/main/java/org/lukas/todoapp/Application.java
+++ backend-java/src/main/java/org/lukas/todoapp/Application.java
@@ -1,7 +1,20 @@
 package org.lukas.todoapp;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
 public class Application {
-    public static void main(String[] args) {
-        // todo
+    private static final int PORT = 8080;
+    public static void main(String[] args) throws IOException {
+        HttpServer httpServer = HttpServer.create(
+                new InetSocketAddress(InetAddress.getLoopbackAddress(), PORT),
+                0
+        );
+
+        System.out.println("HTTP server is starting on port " + PORT);
+        httpServer.start();
     }
 }
