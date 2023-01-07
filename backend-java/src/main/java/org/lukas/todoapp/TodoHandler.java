1cc382ca55f74d58d4821f5833d58412194875b8
Martin Lukas <martinlukas24@protonmail.com>
2023-01-07 15:52:31 +0000

    Implement getting todos in JSON format

--- backend-java/src/main/java/org/lukas/todoapp/TodoHandler.java
+++ backend-java/src/main/java/org/lukas/todoapp/TodoHandler.java
@@ -1,11 +1,21 @@
 package org.lukas.todoapp;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sun.net.httpserver.HttpExchange;
 import org.lukas.todoapp.server.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TodoHandler implements CustomHandler {
+    private final List<Todo> todos;
+
+    public TodoHandler() {
+        this.todos = new ArrayList<>();
+    }
+
     @Override
-    public Response doHandle(HttpExchange exchange) {
+    public Response doHandle(HttpExchange exchange) throws JsonProcessingException {
         return switch (HttpMethod.from(exchange.getRequestMethod())) {
             case GET -> getTodos();
             case POST -> addTodo(exchange);
@@ -13,8 +23,8 @@ public class TodoHandler implements CustomHandler {
         };
     }
 
-    private Response getTodos() {
-        return new Response("hello");
+    private Response getTodos() throws JsonProcessingException {
+        return new Response(objectMapper.writeValueAsString(todos));
     }
 
     private Response addTodo(HttpExchange exchange) {
--- backend-java/src/main/java/org/lukas/todoapp/server/CustomHandler.java
+++ backend-java/src/main/java/org/lukas/todoapp/server/CustomHandler.java
@@ -1,7 +1,10 @@
 package org.lukas.todoapp.server;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 
 public interface CustomHandler {
+    ObjectMapper objectMapper = new ObjectMapper();
+
     Response doHandle(HttpExchange exchange) throws Exception;
 }
