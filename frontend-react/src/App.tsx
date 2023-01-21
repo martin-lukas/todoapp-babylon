ecde7d31ad1668141d077748aadecc6f44cff216
Martin Lukas <martinlukas24@protonmail.com>
2023-01-21 22:35:10 +0000

    Add loaders to app and buttons

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Todo, addTodo, deleteAllTodos, deleteTodo, getTodos } from './service/todoService'
 import './App.css'
 
@@ -6,66 +6,118 @@ function App() {
   const [todos, setTodos] = useState<Todo[]>([])
   const [newTodo, setNewTodo] = useState<string>("")
 
+  const [isAppLoading, setAppLoading] = useState(false)
+  const [isNewLoading, setNewLoading] = useState(false)
+  const [isRemoveLoading, setRemoveLoading] = useState(false)
+  const [isRemoveAllLoading, setRemoveAllLoading] = useState(false)
+
+  const removedTodo = useRef<string | null>(null)
+
   async function fetchTodos() {
     setTodos(await getTodos())
   }
 
   useEffect(() => {
-    fetchTodos()
+    (async () => {
+      setAppLoading(true)
+      await fetchTodos()
+      setAppLoading(false)
+    })()
   }, [])
 
   async function submitAddTodo() {
-    await addTodo({id: null, content: newTodo})
+    setNewLoading(true)
+    await addTodo({ id: null, content: newTodo })
     await fetchTodos()
+    setNewLoading(false)
     setNewTodo('')
   }
 
   async function submitDeleteTodo(todo: Todo) {
+    setRemoveLoading(true)
+    removedTodo.current = todo.id
     await deleteTodo(todo)
     await fetchTodos()
+    setRemoveLoading(false)
+    removedTodo.current = null
     setNewTodo('')
   }
 
   async function submitDeleteAllTodos() {
+    setRemoveAllLoading(true)
     await deleteAllTodos()
     await fetchTodos()
+    setRemoveAllLoading(false)
     setNewTodo('')
   }
 
+  if (isAppLoading) {
+    return <div className="loader"/>
+  }
+
   return (
-    <div>
-      <h1>TODO App</h1>
-      <section className="add-todo-section">
-        <label htmlFor="new-todo">New todo:</label>
-        <input
-          id="new-todo"
-          type="text"
-          value={newTodo}
-          onChange={e => setNewTodo(e.target.value)}
-          onKeyUp={e => e.key === 'Enter' && submitAddTodo()}
-        />
-        <button type="button" onClick={submitAddTodo}>
-          ✔️
-        </button>
-      </section>
-      <section className="todo-list-section">
-        {todos.map(todo => (
-          <div className="todo-item-group" key={todo.id}>
-            <div>
-              <input id="todo-item" type={"checkbox"} key={todo.id}/>
-              <label htmlFor="todo-item">{todo.content}</label>
-            </div>
-            <button type="button" onClick={() => submitDeleteTodo(todo)}>
-              X
-            </button>
-          </div>
-        ))}
-      </section>
-      <section className="global-actions-section">
-        <button type="button" onClick={submitDeleteAllTodos}>
-          Remove All
-        </button>
-      </section>
+    <div className="container">
+      <div className="app-wrapper">
+        <h1>{"TODO App"}</h1>
+        <section className="add-todo-section">
+          <label htmlFor="new-todo">{"New todo:"}</label>
+          <input
+            autoFocus
+            autoComplete="off"
+            id="new-todo"
+            type="text"
+            value={newTodo}
+            onChange={e => setNewTodo(e.target.value)}
+            onKeyUp={e => e.key === 'Enter' && submitAddTodo()}
+          />
+          <button
+            type="button"
+            className="add-todo-btn"
+            onClick={submitAddTodo}
+          >
+            {isNewLoading
+              ? <div className="small-loader"/>
+              : "✔"
+            }
+          </button>
+        </section>
+        {todos.length > 0 && (
+          <>
+            <section className="todo-list-section">
+              {todos.map(todo => (
+                <div className="todo-item-group" key={todo.id}>
+                  <div>
+                    <input id="todo-item" type={"checkbox"} key={todo.id}/>
+                    <label htmlFor="todo-item">{todo.content}</label>
+                  </div>
+                  <button
+                    type="button"
+                    className="center-btn remove-todo-btn"
+                    onClick={() => submitDeleteTodo(todo)}
+                  >
+                    {isRemoveLoading && removedTodo.current === todo.id
+                      ? <div className="small-loader"/>
+                      : "✗"
+                    }
+                  </button>
+                </div>
+              ))}
+            </section>
+            <section className="global-actions-section">
+              <button
+                type="button"
+                className="remove-all-btn"
+                onClick={submitDeleteAllTodos}
+              >
+                {isRemoveAllLoading
+                  ? <div className="small-loader"/>
+                  : "Remove All"
+                }
+              </button>
+            </section>
+          </>
+        )}
+      </div>
     </div>
   )
 }
